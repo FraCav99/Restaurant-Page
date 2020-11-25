73f207b847485a7fb1657302127a62dbe10611a5
FraCav99 <MasterCaster36@outlook.it>
2020-11-25 14:08:59 +0100

    Update gitignore and update dist directory

---
+++ dist/main.js
@@ -0,0 +1,1 @@
+(()=>{"use strict";(()=>{const e=document.querySelector(".hamburger-menu");document.querySelector(".menu-open"),e.addEventListener("click",(()=>{e.classList.toggle("nav-active")}))})()})();
