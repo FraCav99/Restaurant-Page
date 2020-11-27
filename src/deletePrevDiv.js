4fc18fd226633281100ea395d5857d0a2ad0562a
FraCav99 <MasterCaster36@outlook.it>
2020-11-27 18:29:09 +0100

    Delete sources, unpack dist folder for gh-pages branch

--- src/deletePrevDiv.js
+++
@@ -1,6 +0,0 @@
-const removePrevContent = () => {
-    const contentDiv = document.getElementById("content");
-    contentDiv.removeChild(contentDiv.lastChild);
-}
-
-export default removePrevContent;
